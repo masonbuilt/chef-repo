b94a6a183f6fc1d211b8150b3ff902f47645583b
Stripstarter <stripstarter@gmail.com>
2014-09-05 21:28:11 -0700

    Switch out recipes; update README

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,9 @@
-# See http://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
-
-current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mzemel"
-client_key               "#{current_dir}/mzemel.pem"
-validation_client_name   "stripstarter-validator"
-validation_key           "#{current_dir}/stripstarter-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/stripstarter"
-cache_type               'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+node_name                'stripstarter'
+client_key               '/Users/mzemel/Sites/chef-repo/.chef/node3.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/mzemel/Sites/chef-repo/.chef/stripstarter-validator.pem'
+chef_server_url          'https://api.opscode.com/organizations/stripstarter'
+syntax_check_cache_path  '/Users/mzemel/Sites/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '~/Sites/chef-repo//cookbooks' ]
